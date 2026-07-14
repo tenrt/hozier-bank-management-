ffe79eca0898738bd7952c679af14b99bf9ec5e3
tenrt <tengweichoki@gmail.com>
2026-07-14 04:12:40 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const io = new IntersectionObserver((entries) => {
+  entries.forEach(e => {
+    if (e.isIntersecting) {
+      e.target.classList.add('in');
+      io.unobserve(e.target);
+    }
+  });
+}, { threshold: 0.15 });
+
+document.querySelectorAll('[data-reveal]').forEach(el => io.observe(el));
